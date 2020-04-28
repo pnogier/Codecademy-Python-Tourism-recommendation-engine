c56f19e03247b798f2fd047fbdb64d9139948cd3
pnogier <pnogiercontact@gmail.com>
2020-04-28 17:40:03 -0400

    :construction: Adding test objects

---
+++ script.py
@@ -0,0 +1,9 @@
+# Destinations list
+destinations = ["Paris, France",
+                "Shanghai, China",
+                "Los Angeles, USA",
+                "São Paulo, Brazil",
+                "Cairo, Egypt"]
+
+# Test traveler who is in China at the moment
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
